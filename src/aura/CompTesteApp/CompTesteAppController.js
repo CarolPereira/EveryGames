66e92de08e73bf0304a484449ff3b015613b343e
Unknown <carollopes3344@hotmail.com>
2018-01-12 14:28:22 -0200

    Componentes diversos

--- src/aura/CompTesteApp/CompTesteAppController.js
+++ src/aura/CompTesteApp/CompTesteAppController.js
@@ -1,4 +1,24 @@
 ({
-	myAction : function(component, event, helper) {
+	init : function(component, event, helper) {
+		debugger;
+		var idConta = component.get('v.recordId');
+		var buscarContatos = component.get('c.buscarContatos');
+
+		buscarContatos.setParams({
+			'idConta' : idConta
+		});
+
+		buscarContatos.setCallback(this, function(response) {
+			var state = response.getState();
+
+			if(component.isValid() && state === 'SUCCESS') {
+				var contatos;
+
+				contatos = response.getReturnValue();
+				component.set('v.contatos', contatos);
+			}
+		});
+
+		$A.enqueueActionn(buscarContatos);
 	}
 })
